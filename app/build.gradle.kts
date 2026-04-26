cfa17df204952452b9aa5603f48503b545c3f340
PseudonymousEd <pseudonymoused@gmail.com>
2026-04-26 00:49:01 -0700

    Updated apk naming convention

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "0.0.1"
+        setProperty("archivesBaseName", "SongScaffold-$versionName")
     }
 
     buildTypes {
